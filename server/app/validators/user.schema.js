a2ca78b4025e5858dcd0005fa56a4fe7f502670e
Damir.? <vip.ushkempir@mail.ru>
2026-02-12 02:40:06 +0500

    Deleted extra files

--- server/app/validators/user.schema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi");
-
-const updateProfileSchema = Joi.object({
-  name: Joi.string().min(2).max(50).optional(),
-  email: Joi.string().email().optional(),
-  password: Joi.string().min(6).max(72).optional()
-}).min(1);
-
-module.exports = { updateProfileSchema };
